753332511434ced83657ea309d218497e5657a86
Bretton Murphy <djsubaku@gmail.com>
2013-07-07 00:53:59 -0600

    Change urls for logging in

--- time_system/urls.py
+++ time_system/urls.py
@@ -1,31 +1,31 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import RedirectView
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-from dajaxice.core import dajaxice_config, dajaxice_autodiscover
-from settings import APP_PATH, DEBUG
-admin.autodiscover()
-dajaxice_autodiscover()
+#from django.views.generic import RedirectView
+#from django.contrib import admin
+#from dajaxice.core import dajaxice_config, dajaxice_autodiscover
+#from settings import APP_PATH, DEBUG
+#admin.autodiscover()
+#dajaxice_autodiscover()
 
+
+#login views
+urlpatterns = patterns('clocker.views.login',
+    url(r'^$', 'renderLogin', name="render-login"),
+    url(r'^login/$', 'loginUser', name="login"),
+)
+
+
+'''
 #normal views
-urlpatterns = patterns('clocker.views',
-    # Examples:
-    # url(r'^$', 'time_system.views.home', name='home'),
-    # url(r'^time_system/', include('time_system.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    url(r'^timeclock/$', 'main_page'),
-    url(r'^timeclock/hours/$', 'total_hours'),
+urlpatterns += patterns('clocker.views',
+    
     url(r'^$', RedirectView.as_view(url='/timeclock/')),
+    url(r'^timeclock/$', 'main_page'),
 
+    url(r'^timeclock/hours/$', 'total_hours'),
 
-    # Uncomment the next line to enable the admin:
     url(r'(?i)^utilities/', include('chucho.urls')),                                                                                                                                                
     url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
 )
-
 
 if DEBUG:
     urlpatterns += patterns(
@@ -34,13 +34,8 @@ if DEBUG:
             'document_root': APP_PATH + 'chucho/static/plugins/slickGrid/images'})
     )
 
-#login views
-urlpatterns += patterns('clocker.login',
-    url(r'^timeclock/login/$', 'view'),
-    url(r'^logout/$', 'logout'),
-)
-
 #shift_summary stuff
 urlpatterns += patterns('clocker.shift_summary',
     url(r'^timeclock/shift_summary/$', 'summary'),
 )
+'''
